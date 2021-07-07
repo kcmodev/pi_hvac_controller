990a1084aab1a1ee01ac156ad2e8b09d378bcbd7
kcmodev <shchristensen3@gmail.com>
2021-07-07 14:31:25 -0500

    nest api working

--- main.py
+++ main.py
@@ -12,16 +12,18 @@ TODO:
     Determine length of time since it last sprayed
 """
 
+
 def reset_sprayer():
     print('Resetting the sprayer...')
     motor.backward(speed=0.5)
     sleep(0.1)
     motor.stop()
 
+
 def cycle_sprayer_motor():
     # fully cycles sprayer for num sprays
     num_sprays = 2
-    
+
     for i in range(num_sprays):
         print(f'Cycle {i+1} of {num_sprays}')
         motor.forward(speed=0.5)
@@ -30,53 +32,51 @@ def cycle_sprayer_motor():
         sleep(0.3)
         motor.stop()
         sleep(1)
-    
+
+
 def check_time(hours, proc_time):
     # checks time to spray between the hours of
     # 0700 and 2200
     # also checks to cycle only once every 10 minutes
     curr_process_time = proc_time
-    
+
     if 7 < hours < 22:
         print('Currently daytime, cycling sprayer...')
         process_time_diff = curr_process_time - prev_process_time
-        
-        if  process_time_diff <= 60:
+
+        if process_time_diff <= 60:
             print('No spray recently, spraying....')
             prev_process_time = proc_time
-            #cycle_sprayer_motor()
+            # cycle_sprayer_motor()
         else:
-            print(f'Difference in time was only {tim_diff}. NOT spraying...')
+            print(
+                f'Difference in time was only {process_time_diff}. NOT spraying...')
     else:
         print('Currently night time, NOT cycling sprayer...')
-    
+
 
 def spray_the_can():
     # detect how long it has been since it has sprayed
-    
+
     print("spraying the air freshener...")
-    
+
     curr_time = lt()
     curr_hour = curr_time.tm_hour
     curr_min = curr_time.tm_min
-    
+
     print(f'current time: {curr_hour}{curr_min}')
-    
+
     check_time(curr_hour, pt())
-    
+
     sleep(2)
     pir_sensor.wait_for_motion()
 
+
 if __name__ == "__main__":
-    reset_sprayer() # set sprayer to starting position
-    
+    reset_sprayer()  # set sprayer to starting position
+
     try:
         pir_sensor.when_motion = spray_the_can
-        
+
     except KeyboardInterrupt:
         print('User interrupt detected. terminating...')
-        
-        
-        
-        
-        
---
+++ token_stuff.py
@@ -0,0 +1,47 @@
+import requests
+import config
+import json
+
+
+def get_new_token():
+    URL = 'https://www.googleapis.com/oauth2/v4/token'
+
+    PAYLOAD = {
+        'client_id': config.CLIENT_ID,
+        'client_secret': config.CLIENT_SECRET,
+        'refresh_token': config.REFRESH_TOKEN,
+        'grant_type': 'refresh_token'
+    }
+
+    r = requests.post(url=URL, data=PAYLOAD)
+    r = r.json()
+    access_token = r['token_type'] + ' ' + r['access_token']
+
+    print(f'data returned: \n {r}')
+    print(f'NEW access token: \'{access_token}\'')
+
+    # return access_token
+    # get_thermostat_status(access_token)
+    return access_token
+
+
+def get_thermostat_status(x):
+    URL = f'https://smartdevicemanagement.googleapis.com/v1/enterprises/{config.PROJECT_ID}/devices/{config.DEVICE_ID}'
+
+    PAYLOAD = {
+        'Content-Type': 'application/json',
+        'Authorization': x
+    }
+
+    res = requests.get(URL, headers=PAYLOAD)
+    res_json = res.json()
+
+    # print(f'response: \n {json.dumps(res_json, indent=2)}')
+
+    current_status = res_json['traits']['sdm.devices.traits.ThermostatHvac']['status']
+
+    print(f'hvac current status: {current_status}')
+
+
+new_token = get_new_token()
+get_thermostat_status(new_token)
